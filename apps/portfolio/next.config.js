300e1d9af02200b4d94e31af76f4e0b483823dc6
Oleksii Nekryshevych <ne.oleksii@gmail.com>
2026-02-01 19:50:39 +0100

    fix: deploy-github fix

--- apps/portfolio/next.config.js
+++ apps/portfolio/next.config.js
@@ -15,9 +15,9 @@ const nextConfig = {
   },
   // Set basePath for GitHub Pages if deploying to a subdirectory (e.g. /repo-name)
   // Set basePath for GitHub Pages
-  // Check for GITHUB_ACTIONS or NODE_ENV=production
-  basePath: process.env.GITHUB_ACTIONS || process.env.NODE_ENV === 'production' ? '/ostapenko-design' : '',
-  assetPrefix: process.env.GITHUB_ACTIONS || process.env.NODE_ENV === 'production' ? '/ostapenko-design' : '',
+  // Check for NEXT_PUBLIC_BASE_PATH explicity set in deploy.yml
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
 
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
