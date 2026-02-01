5870706d5a6b60df7cb63367e7e07eee3410a306
Oleksii Nekryshevych <ne.oleksii@gmail.com>
2026-02-01 19:55:09 +0100

    fix

--- apps/portfolio/next.config.js
+++ apps/portfolio/next.config.js
@@ -15,9 +15,9 @@ const nextConfig = {
   },
   // Set basePath for GitHub Pages if deploying to a subdirectory (e.g. /repo-name)
   // Set basePath for GitHub Pages
-  // Check for NEXT_PUBLIC_BASE_PATH explicity set in deploy.yml
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  // Check for CI environment (GitHub Actions)
+  basePath: process.env.CI ? '/ostapenko-design' : '',
+  assetPrefix: process.env.CI ? '/ostapenko-design' : '',
 
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
